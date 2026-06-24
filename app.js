995d6d6be763b6a203cff63b075e38bf4a7fddab
David <david@example.com>
2026-06-24 12:03:34 +0200

    coris fixed

--- app.js
+++ app.js
@@ -50,12 +50,8 @@ app.use(
         return callback(null, true)
       }
 
-      if (process.env.NODE_ENV === 'production') {
-        console.warn(`CORS: Allowing unknown origin: ${origin}`)
-        return callback(null, origin)
-      }
-
-      return callback(new Error('Origin not allowed by CORS'))
+      console.warn(`CORS: Allowing unknown origin: ${origin}`)
+      return callback(null, origin)
     },
     credentials: true
   })
